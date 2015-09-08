cddcf38b33963fde55213d1ee335038dcf9a2da4
tomykaira <tomykaira@gmail.com>
2015-09-09 02:55:30 +0900

    Use button

--- rerun/app/src/main/java/com/example/rerun/app/MainActivity.java
+++ rerun/app/src/main/java/com/example/rerun/app/MainActivity.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 import java.util.Date;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import eu.chainfire.libsuperuser.Shell;
 
@@ -15,19 +18,28 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // http://themakeinfo.com/2015/05/building-android-root-app/
-        if (Shell.SU.available()) {
-            Shell.SU.run(Arrays.asList(
-                    "pkill res_mara",
-                    "am force-stop jp.co.bandainamcoent.BNEI0242",
-                    "cp /data/data/jp.co.bandainamcoent.BNEI0242/shared_prefs/jp.co.bandainamcoent.BNEI0242.xml /sdcard/jp.xml" + new Date().getTime(),
-                    "rm /data/data/jp.co.bandainamcoent.BNEI0242/shared_prefs/jp.co.bandainamcoent.BNEI0242.xml",
-                    "cd /sdcard",
-                    "nohup res_mara &"
-            ));
-        } else {
-            Toast.makeText(this, "SU is not available", Toast.LENGTH_LONG).show();
-        }
-        finish();
+        Button button = (Button) findViewById(R.id.run);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // http://themakeinfo.com/2015/05/building-android-root-app/
+                if (Shell.SU.available()) {
+                    Shell.SU.run(Arrays.asList(
+                            "pkill res_mara",
+                            "am force-stop jp.co.bandainamcoent.BNEI0242",
+                            "cp /data/data/jp.co.bandainamcoent.BNEI0242/shared_prefs/jp.co.bandainamcoent.BNEI0242.xml /sdcard/jp.xml" + new Date().getTime(),
+                            "rm /data/data/jp.co.bandainamcoent.BNEI0242/shared_prefs/jp.co.bandainamcoent.BNEI0242.xml"
+                    ));
+                    Intent launchIntent = getPackageManager().getLaunchIntentForPackage("jp.co.bandainamcoent.BNEI0242");
+                    startActivity(launchIntent);
+                    Shell.SU.run(Arrays.asList(
+                            "cd /sdcard",
+                            "nohup res_mara 1 &"
+                    ));
+                } else {
+                    Toast.makeText(MainActivity.this, "SU is not available", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
